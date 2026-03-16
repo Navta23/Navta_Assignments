9b5fcb8079e9928d62cc7449b3524a2b7ddfcc12
Navta23 <guptanavta@gmail.com>
2026-03-16 13:52:54 +0530

    march12assignment

---
+++ March12Assignments/MVCExample/Models/EmpDeptViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCExample.Models
+{
+    public class EmpDeptViewModel
+    {
+        public List<Dept> deptlist {  get; set; }
+        public Employee emp {  get; set; }
+        public DateTime date { get; set; }
+
+    }
+}
