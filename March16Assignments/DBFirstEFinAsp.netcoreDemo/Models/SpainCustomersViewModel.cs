8d29701f7709008e6c7391ac69882c4bb4b64707
Navta23 <guptanavta@gmail.com>
2026-03-18 12:25:49 +0530

    march16assignment

---
+++ March16Assignments/DBFirstEFinAsp.netcoreDemo/Models/SpainCustomersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DBFirstEFinAsp.netcoreDemo.Models
+{
+    public class SpainCustomersViewModel
+    {
+        public string Cid { get; set; }
+        public string Cname { get; set; }
+        public string Comname { get; set; }
+    }
+}
---
+++ March16Assignments/DBFirstEFinAsp.netcoreDemo/Models/View1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBFirstEFinAsp.netcoreDemo.Models;
+
+public partial class View1
+{
+    public string CustomerId { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? ContactName { get; set; }
+
+    public int OrderId { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public decimal? Freight { get; set; }
+
+    public string? City { get; set; }
+}
