6f1ecc97d305714553ec775d12dc8406d45dfabc
Navta23 <guptanavta@gmail.com>
2026-03-24 15:04:11 +0530

    march24assignment

---
+++ March24Assignments/WebApiInAsp.netcore/WebApiInAsp.netcore/Controllers/EmpController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiInAsp.netcore.Models;
+
+namespace WebApiInAsp.netcore.Controllers
+{
+    [Route("api/[controller]")] //url 
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly EmpContext _context;
+        public EmpController(EmpContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetEmployees()
+        {
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("emp2")] //in order to provide each one unique path u have to provide route
+        public List<Employee> GetEmployees2()
+        {
+            return _context.Employees.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployeeById(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> AddEmployee(Employee emp)
+        {
+            await _context.Employees.AddAsync(emp);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpPost]
+        [Route("emp_post2")]
+        public async Task<ActionResult<Employee>> AddEmployee2(Employee emp)
+        {
+            await _context.Employees.AddAsync(emp);
+            await _context.SaveChangesAsync();
+            return Ok(emp);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Employee>>> UpdateEmployee(Employee emp)
+        {
+            var employee = await _context.Employees.FindAsync(emp.Id);
+            if(employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            employee.FirstName = emp.FirstName;
+            employee.LastName = emp.LastName;
+            employee.Email = emp.Email;
+            employee.Age = emp.Age;
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpPut]
+        [Route("put2")]
+        public async Task<ActionResult<Employee>> UpdateEmployee2(Employee emp)
+        {
+            var employee = await _context.Employees.FindAsync(emp.Id);
+            if (employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            employee.FirstName = emp.FirstName;
+            employee.LastName = emp.LastName;
+            employee.Email = emp.Email;
+            employee.Age = emp.Age;
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpPut]
+        [Route("put3")]
+        public async Task<ActionResult<Employee>> UpdateEmployee3(Employee emp, int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            employee.FirstName = emp.FirstName;
+            employee.LastName = emp.LastName;
+            employee.Email = emp.Email;
+            employee.Age = emp.Age;
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Employee>>> DeleteEmployee(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Employees.ToListAsync());
+        }
+
+        [HttpDelete("del2/{id}")]
+        public async Task<ActionResult<Employee>> DeleteEmployee2(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return BadRequest("Employee not found");
+            }
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            return Ok(employee);
+        }
+    }
+}
