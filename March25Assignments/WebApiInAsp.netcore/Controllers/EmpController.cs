b3bb8268efc5025bdd27a40ec41352d10e245ad5
Navta23 <guptanavta@gmail.com>
2026-03-26 11:15:19 +0530

    march25assignment

---
+++ March25Assignments/WebApiInAsp.netcore/Controllers/EmpController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using WebApiInAsp.netcore.Models;
+
+namespace WebApiInAsp.netcore.Controllers
+{
+    [Route("api/[controller]")] //url 
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        private readonly IEmployee _employeeService;
+        public EmpController(IEmployee employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Employee>>> GetAll(int page, int pageSize)
+        {
+            return Ok(await _employeeService.GetAllEmployeesAsync(page, pageSize));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetById(int id)
+        {
+            var employee = await _employeeService.GetEmployeeByIdAsync(id);
+            if(employee == null)
+                return NotFound("Employee not found");
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Employee>>> Add([FromForm]Employee emp, IFormFile image)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var added = await _employeeService.AddEmployeeAsync(emp, image);
+            return Ok(added);
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<Employee>>> Update(int id, [FromForm]Employee emp, IFormFile? image)
+        {
+            if (id != emp.Id)
+            {
+                return BadRequest("Id mismatched!!");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var employee = await _employeeService.UpdateEmployeeAsync(emp, image);
+            if(employee == null)
+                return BadRequest("Employee not found");
+            return Ok(employee);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Employee>>> Delete(int id)
+        {
+            var deleted = await _employeeService.DeleteEmployeeAsync(id);
+            if(deleted == null)
+                return NotFound("Employee not found to delete");
+            return Ok(deleted);
+        }
+    }
+}
---
+++ March25Assignments/WebApiInAsp.netcore/EmployeeService.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiInAsp.netcore.Models;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace WebApiInAsp.netcore
+{
+    public class EmployeeService : IEmployee
+    {
+        private readonly EmpContext _context;
+        private readonly IWebHostEnvironment _env;
+        public EmployeeService(EmpContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public async Task<Employee> AddEmployeeAsync(Employee employee, IFormFile image)
+        {
+            if(image!=null && image.Length > 0)
+            {
+                var imageName = Guid.NewGuid().ToString()+Path.GetExtension(image.FileName); //get the extension like png, jpeg
+                var imagePath = Path.Combine(_env.WebRootPath, "uploads", imageName); // uploads is a folder
+                Directory.CreateDirectory(Path.GetDirectoryName(imagePath)!);
+                using var stream = new FileStream(imagePath, FileMode.Create); // using is used so that we do not need to close the file stream like fs.Close()
+                await image.CopyToAsync(stream);
+                employee.ImagePath = "/uploads/"+ imageName;
+            }
+            await _context.Employees.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return employee;
+        }
+
+        public async Task<Employee?> DeleteEmployeeAsync(int id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if(employee == null)
+            {
+                return null;
+            }
+            DeleteImageFile(employee.ImagePath);
+            _context.Employees.Remove(employee);
+            await _context.SaveChangesAsync();
+            employee.ImagePath = null;
+            return employee;
+        }
+
+        public async Task<List<Employee>> GetAllEmployeesAsync(int pageNumber, int pageSize)
+        {
+
+            return await _context.Employees.
+                 Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<Employee?> GetEmployeeByIdAsync(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        private void DeleteImageFile(string? imagePath)
+        {
+            if (string.IsNullOrEmpty(imagePath))
+                return;
+
+            var fullPath = Path.Combine
+                (_env.WebRootPath, imagePath.TrimStart('/').Replace
+                ('/', Path.DirectorySeparatorChar));
+            if (File.Exists(fullPath))
+            {
+                File.Delete(fullPath);
+            }
+        }
+
+        private string SaveImageToUploads(IFormFile image)
+        {
+            var imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+            var uploadPath = Path.Combine(_env.WebRootPath, "uploads");
+
+            if (!Directory.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            var fullPath = Path.Combine(uploadPath, imageName);
+            using var stream = new FileStream(fullPath, FileMode.Create);
+            image.CopyTo(stream);
+
+            return "/uploads/" + imageName;
+        }
+        public async Task<Employee?> UpdateEmployeeAsync(Employee employee, IFormFile? image) // ? -> you can either update the image or not  your will
+        {
+            var existing = await _context.Employees.FindAsync(employee.Id);
+            if (existing == null) return null;
+
+            existing.FirstName = employee.FirstName;
+            existing.LastName = employee.LastName;
+            existing.Email = employee.Email;
+            existing.Age = employee.Age;
+
+            if (image != null && image.Length > 0)
+            {
+                DeleteImageFile(existing.ImagePath);
+                existing.ImagePath = SaveImageToUploads(image);
+            }
+
+
+            await _context.SaveChangesAsync();
+            return existing;
+        }
+    }
+}
