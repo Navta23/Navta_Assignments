b2966ea7a5acd685538e2da63427b3704aa5461c
Navta23 <guptanavta@gmail.com>
2026-03-28 09:19:10 +0530

    march27assignment

---
+++ March27Assignments/WebApiInAsp.netcore/Models/EmpContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApiInAsp.netcore.Models
+{
+    public class EmpContext: DbContext
+    {
+        public EmpContext(DbContextOptions<EmpContext>
+            dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
