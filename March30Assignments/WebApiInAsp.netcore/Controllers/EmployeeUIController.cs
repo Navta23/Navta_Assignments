ae26b64785c5e512037c50a48e5c135bfe971f14
Navta23 <guptanavta@gmail.com>
2026-03-31 09:15:40 +0530

    march30assignment

---
+++ March30Assignments/WebApiInAsp.netcore/Controllers/EmployeeUIController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiInAsp.netcore.Controllers
+{
+    public class EmployeeUIController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+        
+        public ActionResult Edit(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public IActionResult Export()
+        {
+            return View();
+        }
+
+    }
+}
