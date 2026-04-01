1d4fa46f5ead86b6f3f6ff9526753f48bee87516
Navta23 <guptanavta@gmail.com>
2026-04-01 09:22:20 +0530

    march31assignment

---
+++ March31Assignments/WebApiInAsp.netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApiInAsp.netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
